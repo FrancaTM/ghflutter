311e344b745dd4545b00baa0f6e73370e9735e62
FrancaTM <francatm@outlook.com>
2019-08-28 22:26:57 -0300

    make network call (load data)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 import 'strings.dart';
@@ -20,6 +22,25 @@ class GHFlutter extends StatefulWidget {
 }
 
 class GHFlutterState extends State<GHFlutter> {
+  var _members = [];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  _loadData() async {
+    String dataUrl = 'https://api.github.com/orgs/raywenderlich/members';
+    http.Response response = await http.get(dataUrl);
+    print(response.body);
+    setState(() {
+      _members = json.decode(response.body);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
